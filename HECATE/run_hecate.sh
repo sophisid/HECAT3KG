242f0f6e2d87de8072c65acf845844895f7cf9e0
sophisid <sophiasideris@gmail.com>
2025-04-11 10:02:09 +0300

    fix in mappings

--- HECATE/run_hecate.sh
+++ HECATE/run_hecate.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# java -jar ../../x3ml/target/x3ml-engine-2.2.2-SNAPSHOT-exejar.jar --input HECATE_columns.xml --x3ml mappings/mappings_data_source.x3ml --policy mappings/generator-policies.xml --output HECATE_column_info.trig --format application/trig --reportProgress
- java -jar ../../x3ml/target/x3ml-engine-2.2.2-SNAPSHOT-exejar.jar --input HECATE_columns.xml --x3ml mappings_v2/columns.x3ml --policy mappings_v2/generator-policies.xml --output HECATE_column_info.trig --format application/trig --reportProgress
+
+for i in {1..20474}
+do
+    input_file="../HECATE_columns_part${i}.xml"
+    output_file="../HECATE_part${i}.trig"
+    
+    echo "Processing $input_file to $output_file..."
+    
+    java -Xms10g -Xmx14g -jar ../../x3ml/target/x3ml-engine-2.2.2-SNAPSHOT-exejar.jar \
+        --input "$input_file" \
+        --x3ml mappings_v2/columns.x3ml \
+        --policy mappings_v2/generator-policies.xml \
+        --output "$output_file" \
+        --format application/trig \
+        --reportProgress
+    
+    if [ $? -eq 0 ]; then
+        echo "Successfully processed $input_file to $output_file"
+    else
+        echo "Error processing $input_file"
+    fi
+done
